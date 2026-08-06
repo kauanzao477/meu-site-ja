532eb1d16da5c194402d9f7c7c9d9fb649ad1a7a
kauanzao477 <kauanhenriquesilvabalestrin@gmail.com>
2026-08-06 08:17:31 -0300

    Add files via upload

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,12 +1,42 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore, collection, query, where, getDocs, doc, getDoc, setDoc, deleteDoc } from 'firebase/firestore';
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  getDocs,
+  doc,
+  getDoc,
+  setDoc,
+  deleteDoc
+} from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
-import firebaseConfig from './serviceAccountKey.js';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-export { auth, db, storage, collection, query, where, getDocs, doc, getDoc, setDoc, deleteDoc };
+export {
+  auth,
+  db,
+  storage,
+  collection,
+  query,
+  where,
+  getDocs,
+  doc,
+  getDoc,
+  setDoc,
+  deleteDoc
+};
